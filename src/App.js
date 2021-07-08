0686de5752a2702c6d931bb718c61b4b9b08002a
lexdechris23 <70167097+lexdechris23@users.noreply.github.com>
2021-07-07 20:14:30 -0500

    all working except crossing items off list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,89 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
+    constructor() {
+      super();
+      this.state = {
+        todos: [
+          {
+            task: 'Make Breakfast',
+            id: 1,
+            completed: false,
+          },
+          {
+            task: 'Do Laundey',
+            id: 2,
+            completed: false,
+          },
+          {
+            task: 'Grocery Shopping',
+            id: 3,
+            completed: false,
+          },
+          {
+            task: 'Make Lunch',
+            id: 4,
+            completed: false,
+          },
+          {
+            task: 'Clean Out Garage',
+            id: 5,
+            completed: false,
+          },
+        ]
+      }
+    }
+ 
+
+addItem = (taskName) => {
+  const newTodo = {
+    task: taskName,
+    id: new Date(),
+    completed: false,
+  };
+  this.setState({
+    todos: [...this.state.todos, newTodo],
+  })
+}
+
+toggleItem = (id) => {
+  this.setState({
+    todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        return {
+          ...todo, completed: !todo.completed
+        }
+      } 
+      else {
+        return todo
+      }
+    })
+  })
+}
+
+clearCompleted = event => {
+  event.preventDefault();
+  this.setState({
+    todos: this.state.todos.filter(todo => !todo.completed) 
+  })
+}
+
+render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+          <h2>Chores</h2>
+          <TodoList todos = {this.state.todos} toggleItem = {this.toggleItem} />
+          <TodoForm addItem = {this.addItem} />
+          <button onClick = {this.clearCompleted}> Finished </button>
       </div>
-    );
-  }
+    )
+}
 }
 
 export default App;
